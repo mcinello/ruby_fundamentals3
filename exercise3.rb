fe9d566e2de5928c8457882b95300a49ecb17cb4
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:50:13 -0400

    Printed out each movie and the year it came out

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,13 @@
-#Array of favourite performing artists
-fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
+# #Array of favourite performing artists
+# fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
+#
+# #print out first 2 performing artistd
+# print "First two bands are #{fav_bands[0]} and #{fav_bands[1]}"
 
-#print out first 2 performing artistd
-print "First two bands are #{fav_bands[0]} and #{fav_bands[1]}"
+#Hash of favourite move names and year of creation
+fav_movies = {HP: 2001, LOTR: 2001, Clueless: 1995, Mulan: 1998, Tangled: 2010}
+
+#Print out each movie and when it came out
+fav_movies.each do |movie, year|
+  puts "#{movie} came out in #{year}"
+end
