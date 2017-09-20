22059ccf2480788f8f1ad5536b29e8305bcdbf52
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:19:31 -0400

    corrected way to print last array

--- exercise2.rb
+++ exercise2.rb
@@ -2,4 +2,5 @@
 fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
 
 #Print last colour
-print "My last favourite colour is #{fav_colours[4]}"
+print "My last favourite colour is #{fav_colours.last}"
+# or fav_colours[-1]
