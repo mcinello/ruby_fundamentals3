928ab44350a590e09ea00a2a9dfa4d20772d7c6a
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 16:56:05 -0400

    Created hashes from the list of items

--- exercise0.rb
+++ exercise0.rb
@@ -1,4 +1,4 @@
-#1 CREATING ARRAYS
+#1 CREATING ARRAYS ---------------------------
 
 #Array of favourite colours
 fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
@@ -13,4 +13,19 @@ coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
 
 #Array of fav colours as SYMBOLS
-fav_colours_symbols = [:pink, :blue, :red, :green:, :turquoise]
+fav_colours_symbols = [:pink, :blue, :red, :green, :turquoise]
+
+#2 CREATING HASHES ------------------------------
+
+#Hash of words and their definitions
+definitions = {mom: 'maternal figure', dad: 'paternal figure', brother: 'sibling'}
+
+#Hash of favourite move names and year of creation
+fav_movies = {HP: 2001, LOTR: 2001, Clueless: 1995, Mulan: 1998, Tangled: 2010}
+
+#Hash of 3 cities of the world and their population
+
+cities = {Toronto: '2.615 million', Seoul: '10.58 million', Tokyo: '13.62 million'}
+
+#Family members and their age
+family_members = {Josh: 29, Isabella: 21, Victoria: 23, Audrey: 16, Vincent: 13, Luca: 39}
