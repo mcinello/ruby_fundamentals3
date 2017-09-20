36e7c8113808518a0f1a0faa1d37534a4cbf9f3b
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:01:15 -0400

    Printed array of coin flips

---
+++ exercise1.rb
@@ -0,0 +1,11 @@
+#Array of Heads or Tails
+coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
+
+#1. Print out array of coin flips
+  print "Results from the coin toss: #{coin_toss}"
+
+# #Array of family member's age
+# family_age = [26, 29, 21, 56, 57]
+#
+# #Hash of favourite move names and year of creation
+# fav_movies = {HP: 2001, LOTR: 2001, Clueless: 1995, Mulan: 1998, Tangled: 2010}
