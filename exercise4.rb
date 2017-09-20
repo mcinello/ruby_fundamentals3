6388ef6ca00597e40ae34eac2e3fa6910cdf2571
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 18:44:55 -0400

    print out ages under 30

---
+++ exercise4.rb
@@ -0,0 +1,9 @@
+#Array of family member's age
+family_age = [26, 29, 21, 56, 57]
+
+#Print out ages under 30
+young_fam = family_age.select do |num|
+  young_age = num < 30
+end
+
+  puts "Age of young people in the family are #{young_fam}"
