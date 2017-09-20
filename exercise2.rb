dfc0675c3688b1bfc295a89a4280629f87030fcf
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:42:31 -0400

    Print out sentence with each array

--- exercise2.rb
+++ exercise2.rb
@@ -6,16 +6,24 @@
 # # or fav_colours[-1]
 
 # #Hash of 3 cities of the world and their population
-cities = {Toronto: '2.615 million', Seoul: '10.58 million', Tokyo: '13.62 million'}
+# cities = {Toronto: '2.615 million', Seoul: '10.58 million', Tokyo: '13.62 million'}
+# #
+# # #Add a new city and its population
+# cities[:London] = '8.788 million'
 #
-# #Add a new city and its population
-cities[:London] = '8.788 million'
-
-#Print population of one of the cities
-print "Seoul has a population of #{cities[:Seoul]}"
+# #Print population of one of the cities
+# print "Seoul has a population of #{cities[:Seoul]}"
 
 #Array of Heads or Tails
 # coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 
 #reverse coin flips
 # coin_toss.reverse
+
+#Array of favourite performing artists
+fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
+
+#print out sentence for each array
+fav_bands.map do |band|
+  print "I think #{band} is great"
+end
