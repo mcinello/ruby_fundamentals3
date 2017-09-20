9a158c8a5adffd2a96fbdccca3826cbf74f7ec51
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:04:55 -0400

    Printed first element of array of fav_colours

--- exercise1.rb
+++ exercise1.rb
@@ -1,8 +1,19 @@
+=begin
 #Array of Heads or Tails
 coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 
 #1. Print out array of coin flips
   print "Results from the coin toss: #{coin_toss}"
+=end
+
+
+
+#Array of favourite colours
+fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+
+#2. Print out the first element of the array of your favourite colours
+
+print "My first favourite colour is #{fav_colours[0]}"
 
 # #Array of family member's age
 # family_age = [26, 29, 21, 56, 57]
