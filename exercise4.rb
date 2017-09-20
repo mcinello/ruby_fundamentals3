9f6b47bf1bf49d260d2d311b5c30e14f6b1e16b5
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 18:53:12 -0400

    Counted number of times 'heads' was flipped in array

--- exercise4.rb
+++ exercise4.rb
@@ -1,14 +1,19 @@
-#Array of family member's age
-family_age = [26, 29, 21, 56, 57]
-
-# #Print out ages under 30
-# young_fam = family_age.select do |num|
-#   young_age = num < 30
-# end
-# 
-# puts "Age of young people in the family are #{young_fam}"
-
-#Output age of oldest person in family_age Array
-puts family_age.each.max
+# #Array of family member's age
+# family_age = [26, 29, 21, 56, 57]
+#
+# # #Print out ages under 30
+# # young_fam = family_age.select do |num|
+# #   young_age = num < 30
+# # end
+# #
+# # puts "Age of young people in the family are #{young_fam}"
+#
+# #Output age of oldest person in family_age Array
+# puts family_age.each.max
 
 # ------------------------------------------------------------
+#Array of Heads or Tails
+coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
+
+#count how many times 'heads' was flipped
+coin_toss.count('heads')
