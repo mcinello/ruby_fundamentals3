d770c4b836889ae0434d3ab3d1ccc58f7bc3f1be
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:16:27 -0400

    comments taken off

--- exercise1.rb
+++ exercise1.rb
@@ -1,30 +1,25 @@
-=begin
+
 #Array of Heads or Tails
 coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 
 #1. Print out array of coin flips
   print "Results from the coin toss: #{coin_toss}"
-=end
 
-
-=begin
 #Array of favourite colours
 fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
 
 #2. Print out the first element of the array of your favourite colours
 
 print "My first favourite colour is #{fav_colours[0]}"
-=end
+
 
 # #Array of family member's age
-=begin
 family_age = [26, 29, 21, 56, 57]
 
 family_age.push << 0
 
 #Output sorted version of array of friends and family members ages.
 print "Youngest to oldest, my family's ages #{family_age.sort}"
-=end
 
 
 # #Hash of favourite move names and year of creation
