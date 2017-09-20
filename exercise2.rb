98709b4b6dbb2ae36ad2ab5ebc704117de2fa9a5
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:44:22 -0400

    uncommented code and sectioned off in .rb file

--- exercise2.rb
+++ exercise2.rb
@@ -1,24 +1,36 @@
 # #Array of favourite colours
-# fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
 #
 # #Print last colour
-# print "My last favourite colour is #{fav_colours.last}"
+print "My last favourite colour is #{fav_colours.last}"
 # # or fav_colours[-1]
 
+
+# ----------------------------------------------------------------------
+
+
 # #Hash of 3 cities of the world and their population
-# cities = {Toronto: '2.615 million', Seoul: '10.58 million', Tokyo: '13.62 million'}
+cities = {Toronto: '2.615 million', Seoul: '10.58 million', Tokyo: '13.62 million'}
 # #
 # # #Add a new city and its population
-# cities[:London] = '8.788 million'
+cities[:London] = '8.788 million'
 #
 # #Print population of one of the cities
-# print "Seoul has a population of #{cities[:Seoul]}"
+print "Seoul has a population of #{cities[:Seoul]}"
+
+
+# ------------------------------------------------------------------------
+
 
 #Array of Heads or Tails
-# coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
+coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 
 #reverse coin flips
-# coin_toss.reverse
+coin_toss.reverse
+
+
+# ------------------------------------------------------------------------
+
 
 #Array of favourite performing artists
 fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
