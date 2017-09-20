381a9b2bf5dd23f8d6e4a9298563b7ec68d20d09
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 16:43:29 -0400

    Created arrays for each item

---
+++ exercise0.rb
@@ -0,0 +1,16 @@
+#1 CREATING ARRAYS
+
+#Array of favourite colours
+fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+
+#Array of family member's age
+family_age = [26, 29, 21, 56, 57]
+
+#Array of Heads or Tails
+coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
+
+#Array of favourite performing artists
+fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
+
+#Array of fav colours as SYMBOLS
+fav_colours_symbols = [:pink, :blue, :red, :green:, :turquoise]
