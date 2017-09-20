badf3711de3f6f76b1219a3787f143bbe2d91c97
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 17:07:40 -0400

    Sorted family members ages

--- exercise1.rb
+++ exercise1.rb
@@ -7,16 +7,22 @@ coin_toss = ['heads', 'tails', 'tails', 'heads', 'heads']
 =end
 
 
-
+=begin
 #Array of favourite colours
 fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
 
 #2. Print out the first element of the array of your favourite colours
 
 print "My first favourite colour is #{fav_colours[0]}"
+=end
 
 # #Array of family member's age
-# family_age = [26, 29, 21, 56, 57]
-#
+family_age = [26, 29, 21, 56, 57]
+
+#Output sorted version of array of friends and family members ages.
+print "Youngest to oldest, my family's ages #{family_age.sort}"
+
+
+
 # #Hash of favourite move names and year of creation
 # fav_movies = {HP: 2001, LOTR: 2001, Clueless: 1995, Mulan: 1998, Tangled: 2010}
