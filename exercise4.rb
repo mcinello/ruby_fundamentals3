67b4b64568e58797328d1f8091300580744428bb
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 18:50:26 -0400

    Output age of oldest person in family_age array

--- exercise4.rb
+++ exercise4.rb
@@ -1,9 +1,14 @@
 #Array of family member's age
 family_age = [26, 29, 21, 56, 57]
 
-#Print out ages under 30
-young_fam = family_age.select do |num|
-  young_age = num < 30
-end
+# #Print out ages under 30
+# young_fam = family_age.select do |num|
+#   young_age = num < 30
+# end
+# 
+# puts "Age of young people in the family are #{young_fam}"
 
-  puts "Age of young people in the family are #{young_fam}"
+#Output age of oldest person in family_age Array
+puts family_age.each.max
+
+# ------------------------------------------------------------
