b6850de1b188eab6ec82ec9e79439f02efef861a
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 21:37:22 -0400

    Added one year to age arrays

--- exercise5.rb
+++ exercise5.rb
@@ -26,13 +26,22 @@ cities = {Toronto: 2731571, Seoul: 9914381, Tokyo: 13617445}
 # end
 
 # --------------------------------------------------------------------------
-#Array of favourite colours
-fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
-
-last = fav_colours.last
-second_last = fav_colours[fav_colours.length - 2]
-
-#print last 2 colours
-puts "#{last} #{second_last}"
+# #Array of favourite colours
+# fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+#
+# last = fav_colours.last
+# second_last = fav_colours[fav_colours.length - 2]
+#
+# #print last 2 colours
+# puts "#{last} #{second_last}"
 
 # --------------------------------------------------------------------------------
+
+#Array of family member's age
+family_age = [26, 29, 21, 56, 57]
+
+#Add 1 to everyone's age
+add_one = family_age.map { |age| age + 1}
+prints "New age #{add_one}"
+
+# --------------------------------------------------------------------------
