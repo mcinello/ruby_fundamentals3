72a98676e40f2ad95a26b65c9f86f31db6781b20
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 00:14:18 -0400

    uncommented code

--- exercise6-part2.rb
+++ exercise6-part2.rb
@@ -1,22 +1,22 @@
 # #Repeat sentence 20 times
-# message_array = []
-# 20.times do
-#   message_array.push("I will not skateboard in the halls")
-# end
+message_array = []
+20.times do
+  message_array.push("I will not skateboard in the halls")
+end
 
 #create array with numbers 1-50
-# numbers = []
-# num = 0
-# until num == 50
-#   num += 1
-#   3.times do
-#     numbers.push(num)
-#   end
-# end
+numbers = []
+num = 0
+until num == 50
+  num += 1
+  3.times do
+    numbers.push(num)
+  end
+end
 
 #use each loop to find sum
-# sum = 0
-# numbers.each { |number| sum+=number }
+sum = 0
+numbers.each { |number| sum+=number }
 
 countries = [
   {name: 'Bulgaria', continent: 'Europe', island?: 'no'},
@@ -31,4 +31,3 @@ countries.each { |country|
   end
 }
 puts not_islands
-#print no_islands = [country_one, country_two]
