b1851bab59e87000cc9abdf41245d217624d4da1
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 23:10:32 -0400

    created array with numbers 1-50

--- exercise6-part2.rb
+++ exercise6-part2.rb
@@ -1,5 +1,11 @@
-#Repeat sentence 20 times
-message_array = []
-20.times do
-  message_array.push("I will not skateboard in the halls")
+# #Repeat sentence 20 times
+# message_array = []
+# 20.times do
+#   message_array.push("I will not skateboard in the halls")
+# end
+
+numbers = []
+num = 0
+until num == 50
+  numbers.push(num += 1)
 end
