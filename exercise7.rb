29772c028c5c2516e4f5493c28741a6e8a52b114
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 01:16:57 -0400

    exercise7.rb output the result for the first set of expenses. 2nd part of exercise  i created the method to reuse

--- exercise7.rb
+++ exercise7.rb
@@ -1,8 +1,25 @@
 expenses = [250, 7.95, 30.95, 16.50]
-result = 0
+expenses_two = [300, 8.95, 25.95, 27.50]
 
+# -----------------------------------------------------------------
+
+
+result = 0
+#
 expenses.each do |expense|
-  result += expense.to_i
+  result += expense.to_i #adds up result
 end
 
 puts result
+
+# --------------------------------------------------------------------
+
+def sum(expenses)
+  result = 0
+  expenses.each do |expense|
+    result += expense.to_i #adds up result
+  end
+  return result
+end
+
+puts total_expenses = sum(expenses_two)
