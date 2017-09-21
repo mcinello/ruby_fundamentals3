db7fd5daef5b353f02a94972df0a3b037e42e888
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 23:15:56 -0400

    Used .each loop to find sum of array

--- exercise6-part2.rb
+++ exercise6-part2.rb
@@ -4,8 +4,13 @@
 #   message_array.push("I will not skateboard in the halls")
 # end
 
+#create array with numbesr 1-50
 numbers = []
 num = 0
 until num == 50
   numbers.push(num += 1)
 end
+
+#use each loop to find sum
+sum = 0
+numbers.each { |number| sum+=number }
