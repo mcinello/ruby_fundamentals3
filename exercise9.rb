93942eecdcb63e7fed2d1070f11be1fa4b24406f
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 16:07:31 -0400

    modified the students hash to equate the class increase by 5%, then found the sum of all students in classes with .each

--- exercise9.rb
+++ exercise9.rb
@@ -9,11 +9,21 @@ def add_cohort(students, cohort, amount)
   students.delete(:cohort2) #deletes 2nd cohort
   students.each { |cohort, amount| #displays cohort
     student_increase = amount * 1.05 #adds 5% to all cohorts
+    students[cohort] = student_increase
     puts "#{cohort}: #{student_increase} students" }
 end
 
 add_new_cohort = add_cohort(students, :cohort4, 43)
 
 puts cohort_numbers = students.keys #output cohort names
+#
+# #total number of students
+def total_students(students)
+  total = 0
+  students.each_value do |value|
+    total += value
+  end
+  return total
+end
 
-#increase each cohort by 5%
+puts all_students = total_students(students)
