8c9cf225c16692d8a830abbb150753df6b86ff4c
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 20:59:26 -0400

    printed sentences based on family members age

--- exercise5.rb
+++ exercise5.rb
@@ -1,12 +1,27 @@
 #Hash of 3 cities of the world and their population
 cities = {Toronto: 2731571, Seoul: 9914381, Tokyo: 13617445}
 
-#Sum total of all the populations
-total = 0
-cities.each do |city, population|
- total += population.to_i
+# #Sum total of all the populations
+# total = 0
+# cities.each do |city, population|
+#  total += population.to_i
+# end
+#
+# print "The total population is #{total}"
+#
+# # -------------------------------------------------------------------------
+
+#Family members and their age
+family_members = {Josh: 29, Isabella: 21, Victoria: 23, Audrey: 16, Vincent: 13, Luca: 39}
+
+family_members.select do |name, age|
+    if age > 26
+      puts "#{name} is oldish."
+    elsif young_adult_age = age <= 25
+      puts "#{name} is getting there."
+    elsif teen_age = age < 20
+      puts "#{name} is young."
+  end
 end
 
-print "The total population is #{total}"
-
-# -------------------------------------------------------------------------
+# --------------------------------------------------------------------------
