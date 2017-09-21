ad068b12622c0ecb0361a4629860bb21ea73cdd9
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 21:31:13 -0400

    Printed last 2 colours

--- exercise5.rb
+++ exercise5.rb
@@ -12,16 +12,27 @@ cities = {Toronto: 2731571, Seoul: 9914381, Tokyo: 13617445}
 # # -------------------------------------------------------------------------
 
 #Family members and their age
-family_members = {Josh: 29, Isabella: 21, Victoria: 23, Audrey: 16, Vincent: 13, Luca: 39}
-
-family_members.select do |name, age|
-    if age > 26
-      puts "#{name} is oldish."
-    elsif young_adult_age = age <= 25
-      puts "#{name} is getting there."
-    elsif teen_age = age < 20
-      puts "#{name} is young."
-  end
-end
+# family_members = {Josh: 29, Isabella: 21, Victoria: 23, Audrey: 16, Vincent: 13, Luca: 39}
+#
+# #print out sentences based on age
+# family_members.select do |name, age|
+#     if age > 26
+#       puts "#{name} is oldish."
+#     elsif young_adult_age = age <= 25
+#       puts "#{name} is getting there."
+#     elsif teen_age = age < 20
+#       puts "#{name} is young."
+#   end
+# end
 
 # --------------------------------------------------------------------------
+#Array of favourite colours
+fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+
+last = fav_colours.last
+second_last = fav_colours[fav_colours.length - 2]
+
+#print last 2 colours
+puts "#{last} #{second_last}"
+
+# --------------------------------------------------------------------------------
