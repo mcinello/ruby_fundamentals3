73a17477a0c6d4c5d61cf6147cd4944debca560f
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 23:54:40 -0400

    created three_numbers array that loops 1-50 three times

--- exercise6-part2.rb
+++ exercise6-part2.rb
@@ -4,13 +4,18 @@
 #   message_array.push("I will not skateboard in the halls")
 # end
 
-#create array with numbesr 1-50
+#create array with numbers 1-50
 numbers = []
 num = 0
 until num == 50
-  numbers.push(num += 1)
+  num += 1
+  3.times do
+    numbers.push(num)
+  end
 end
 
 #use each loop to find sum
-sum = 0
-numbers.each { |number| sum+=number }
+# sum = 0
+# numbers.each { |number| sum+=number }
+
+#array with 3 of each number
