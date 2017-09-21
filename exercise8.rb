0ec2d4e44eec376f87fc04eb3132ef66dd26f711
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 10:17:49 -0400

    moved grocery_list into the method b/c it needs to be a local defined variable. Added rice to the list

--- exercise8.rb
+++ exercise8.rb
@@ -1,3 +1,8 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+def add_list(grocery)
+  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  grocery_list.push(grocery)
+  grocery_list.each { |item|
+    puts "*#{item}" }
+end
 
-grocery_list.each { |item| puts "*#{item}" }
+grocery_list = add_list("rice")
