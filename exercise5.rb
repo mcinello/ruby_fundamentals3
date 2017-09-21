458fcbab7f8502fad8987fa13d3625a430e9b70e
Michelle Cinello <m.cinello@gmail.com>
2017-09-20 21:42:45 -0400

    Added 2 colours

--- exercise5.rb
+++ exercise5.rb
@@ -26,8 +26,8 @@ cities = {Toronto: 2731571, Seoul: 9914381, Tokyo: 13617445}
 # end
 
 # --------------------------------------------------------------------------
-# #Array of favourite colours
-# fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
+#Array of favourite colours
+fav_colours = ['pink', 'blue', 'red', 'green', 'turquoise']
 #
 # last = fav_colours.last
 # second_last = fav_colours[fav_colours.length - 2]
@@ -35,13 +35,16 @@ cities = {Toronto: 2731571, Seoul: 9914381, Tokyo: 13617445}
 # #print last 2 colours
 # puts "#{last} #{second_last}"
 
+#Add 2 colours
+fav_colours.push('violet', 'magenta')
+
 # --------------------------------------------------------------------------------
 
-#Array of family member's age
-family_age = [26, 29, 21, 56, 57]
-
-#Add 1 to everyone's age
-add_one = family_age.map { |age| age + 1}
-prints "New age #{add_one}"
+# #Array of family member's age
+# family_age = [26, 29, 21, 56, 57]
+#
+# #Add 1 to everyone's age
+# add_one = family_age.map { |age| age + 1}
+# prints "New age #{add_one}"
 
 # --------------------------------------------------------------------------
