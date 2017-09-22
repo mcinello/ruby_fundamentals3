78a3d930728bceb976a94bd81c3f90edb7534866
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 23:16:14 -0400

    Added 1 year to family_age array

--- exercise12.rb
+++ exercise12.rb
@@ -15,3 +15,10 @@ fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
 family_age.zip(fav_bands).each do |age, band|
   puts "I <3 #{band} #{age}"
 end
+
+# ----------------------------------------------------
+
+family_age = [26, 29, 21, 56, 57]
+
+family_age.map! { |age| age + 1}
+print "New age #{family_age}"
