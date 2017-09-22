14b248ae4afb8add83471f4c792aebfe7d58f5f9
Michelle Cinello <m.cinello@gmail.com>
2017-09-21 23:10:58 -0400

    displayed 2 arrays as a pair

--- exercise12.rb
+++ exercise12.rb
@@ -1,5 +1,17 @@
 #Array of favourite colours
 fav_things = ['pink', 'blue', 'red', 'green', 'turquoise', 'Sum41', 'Blink182', 'Linkin Park']
-
+#
 fav_things.sort_by! { |word| word.downcase }
 puts fav_things
+#
+# # ----------------------------------------------------
+#
+# #Array of family member's age
+family_age = [26, 29, 21, 56, 57]
+
+#Array of favourite performing artists
+fav_bands = ['Sum41', 'Blink182', 'Linkin Park']
+
+family_age.zip(fav_bands).each do |age, band|
+  puts "I <3 #{band} #{age}"
+end
